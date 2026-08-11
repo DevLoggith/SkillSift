756f527e87c47a37afc1133a78e9be76dd751eaa
logan <92327020+DevLoggith@users.noreply.github.com>
2026-08-11 10:46:36 -0400

    convert vanilla css styles & rules to tailwind utility classes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,9 @@ if (!rootElement) {
   throw new Error("Failed to find the root element");
 }
 
+rootElement.className =
+  "w-[1126px] max-w-full mx-auto text-center border-x border-border min-h-svh flex flex-col box-border";
+
 createRoot(rootElement).render(
   <StrictMode>
     <App />
