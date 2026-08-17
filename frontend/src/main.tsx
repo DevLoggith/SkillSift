e7cba6d3cf2c57158e94d91cfc8612fdc953313d
logan <92327020+DevLoggith@users.noreply.github.com>
2026-08-17 16:44:47 -0400

    remove boilerplate & assets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,9 +9,6 @@ if (!rootElement) {
   throw new Error("Failed to find the root element");
 }
 
-rootElement.className =
-  "w-[1126px] max-w-full mx-auto text-center border-x border-border min-h-svh flex flex-col box-border";
-
 createRoot(rootElement).render(
   <StrictMode>
     <App />
