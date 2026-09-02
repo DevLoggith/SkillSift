bb1be058f928bedaecbd0359384cbc2bb9b513ad
logan <92327020+DevLoggith@users.noreply.github.com>
2026-09-02 18:13:28 -0400

    create catchall route and basic homepage navigation on 404 page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
     <main>
       <h1>404 Page Not Found</h1>
+      <Link to={"/"}>
+        <button type="button">Go Back Home</button>
+      </Link>
     </main>
   );
 }
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -3,12 +3,14 @@ import App from "../App";
 import About from "../pages/About";
 import Charts from "../pages/Charts";
 import List from "../pages/List";
+import NotFound from "../pages/NotFound";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/list", element: <List /> },
   { path: "/charts", element: <Charts /> },
   { path: "/about", element: <About /> },
+  { path: "*", element: <NotFound /> },
 ]);
 
 export default router;
