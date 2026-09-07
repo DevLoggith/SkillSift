b1202fc6877567c53a367190e8f1d08571fe7e96
logan <92327020+DevLoggith@users.noreply.github.com>
2026-09-07 14:42:14 -0400

    update AppRoutes component to add RootLayout & children attribute

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import RootLayout from "../components/RootLayout";
 import About from "../pages/About";
 import Charts from "../pages/Charts";
+import Home from "../pages/Home";
 import List from "../pages/List";
 import NotFound from "../pages/NotFound";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/list", element: <List /> },
-  { path: "/charts", element: <Charts /> },
-  { path: "/about", element: <About /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "list", element: <List /> },
+      { path: "charts", element: <Charts /> },
+      { path: "about", element: <About /> },
+    ],
+  },
   { path: "*", element: <NotFound /> },
 ]);
 
